4f75efe9e09f0fb268b85b120a7a778ae93258b0
bobbycox69 <105347488+bobbycox69@users.noreply.github.com>
2022-07-05 13:59:23 +0200

    Add files via upload

---
+++ updatedregform.py
@@ -0,0 +1,131 @@
+from datetime import datetime, timedelta
+from tkinter import *
+from tkinter import messagebox
+
+import pandas as pd
+import pyodbc
+from PIL import Image, ImageTk
+
+
+class Window():
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(master, width=600, height=400)
+        self.frame.pack()
+        self.frame.place(anchor='center', relx=0.5, rely=0.5)
+
+# Create an object of tkinter ImageTk
+        self.path = 'S:/Öffentliche Ordner/Logos/Core Solution/Logo/CoreSolution_Logo_RGB.jpg'
+        self.img = Image.open(self.path)
+        self.img.thumbnail((150,150))
+        self.new_img = ImageTk.PhotoImage(self.img)
+# Create a Label Widget to display the text or Image
+        self.label = Label(root, image = self.new_img)
+        self.label.pack()
+        self.Main = Frame(self.master)
+
+        
+        # ----- Section 1
+ 
+        self.section1 = Frame(self.Main)
+ 
+
+        self.L1 = Label(self.section1, text = "Personal-Nr")
+        self.L1.pack(padx = 5, pady = 5, side = LEFT)
+ 
+        self.nEmployee = Entry(self.section1)
+        self.nEmployee.pack(padx = 5, pady = 5, side = LEFT)
+ 
+
+        self.L2 = Label(self.section1, text = "Urlaub am/vom")
+        self.L2.pack(padx = 5, pady = 5, side = LEFT)
+ 
+        self.dDateStart = Entry(self.section1)
+        self.dDateStart.pack(padx = 5, pady = 5, side = LEFT)
+
+
+        self.section1.pack(padx = 5, pady = 5, expand = True, fill = X)
+ 
+        # ----- Section 1
+ 
+
+        # ----- Section 2
+ 
+        self.section2 = Frame(self.Main)
+        
+
+        self.L3 = Label(self.section2, text = "bis einschl.")
+        self.L3.pack(padx = 5, pady = 5, side = LEFT)
+
+        self.dDateEnd = Entry(self.section2)
+        self.dDateEnd.pack(padx = 5, pady = 5, side = LEFT)
+        
+
+        self.L4 = Label(self.section2, text = "Stellvertreter")
+        self.L4.pack(padx = 5, pady = 5, side = LEFT)
+       
+        self.E4 = Entry(self.section2)
+        self.E4.pack(padx = 5, pady = 5, side = LEFT)
+        
+
+        self.section2.pack(padx = 5, pady = 5, expand = True, fill = X)
+ 
+        # ----- Section 2
+
+
+        # ----- Section 3
+        
+        self.section3 = Frame(self.Main)
+    
+
+        self.L5 = Label(self.section3, text = "Urlaubsgrund")
+        self.L5.pack(padx = 5, pady = 5)
+
+        self.T1 = Text(self.section3, height = 2, width = 20)
+        self.T1.pack(padx =5, pady = 5, expand = True, fill = X)
+  
+         
+        self.section3.pack(padx = 5, pady = 5, expand = True, fill = X)
+       
+        # ----- Section 3
+        
+        self.B0 = Button(self.Main, text = "Submit", command = lambda : [self.submit(), self.click_me()])
+        self.B0.pack(padx = 5, pady = 5, side = RIGHT)
+ 
+        self.Main.pack(padx = 5, pady = 5, expand = True, fill = X)
+ 
+
+    def submit(self):
+        try:
+            self.cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                                    "Server=SOPCP01DE;"
+                                    "Database=PulseCoreTest5;"
+                                    "UID=PCDev2;"
+                                    "PWD=PCCSDev2PC5_!")
+            self.cursor = self.cnxn.cursor()
+            print("Connection Succeeded")
+        except:
+            print("Connection Failed")
+
+        start_date = self.dDateStart.get().strip()
+        end_date = self.dDateEnd.get().strip()
+        if start_date or end_date:
+            if end_date == '':
+                end_date = start_date
+            sql = """INSERT INTO dbo.PC_VacationsRequests (dDateStart, dDateEnd, nEmployee, sReasons, sStatus, sStellvertreter)
+                    VALUES (?, ?, ?, ?, ?, ?)"""
+            values = (start_date, end_date, self.nEmployee.get(), self.T1.get("1.0", "end"), "geplant", self.E4.get())
+        
+            self.cursor.execute(sql, values)
+            self.cnxn.commit() 
+            self.cnxn.close()
+    
+
+    def click_me(self):
+        self.mssg = messagebox.showinfo("Success", "Your submission was recorded!")
+    
+        
+root = Tk()
+root.resizable(False, False)
+window = Window(root)
+root.mainloop()
