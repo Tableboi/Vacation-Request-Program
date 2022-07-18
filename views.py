2bb0906791e8a9b7f1214f0a69ab840c39a28efb
rebeccafran <105347198+rebeccafran@users.noreply.github.com>
2022-07-18 13:14:47 +0200

    Update views.py

--- views.py
+++ views.py
@@ -32,13 +32,13 @@ class loginbox(ttk.Frame):
         self.top_frame.pack(side = 'top', fill = 'x')
 
         # Create an object of tkinter ImageTk
-        #self.path = 'S:/Öffentliche Ordner/Logos/Core Solution/Logo/CoreSolution_Logo_RGB_negativ.png'
-        #self.img = Image.open(self.path)
-        #self.img.thumbnail((200,200))
-        #self.new_img = ImageTk.PhotoImage(self.img)
+        self.path = 'S:/Öffentliche Ordner/Logos/Core Solution/Logo/CoreSolution_Logo_RGB_negativ.png'
+        self.img = Image.open(self.path)
+        self.img.thumbnail((200,200))
+        self.new_img = ImageTk.PhotoImage(self.img)
         # Create a Label Widget to display the text or Image
-        #self.label = ttk.Label(self.top_frame, image = self.new_img)
-        #self.label.pack(side = 'right')
+        self.label = ttk.Label(self.top_frame, image = self.new_img)
+        self.label.pack(side = 'right')
 
         #setting the font types
         header_font = tkinter.font.Font(\
@@ -411,13 +411,13 @@ class request_window(ttk.Frame):
         self.title_label.grid(column = 0, row = 0, padx = 5, pady = 5)
 
         # Create an object of tkinter ImageTk
-        #self.path = 'S:/Öffentliche Ordner/Logos/Core Solution/Logo/CoreSolution_Logo_RGB_negativ.png'
-        #self.img = Image.open(self.path)
-        #self.img.thumbnail((200,200))
-        #self.new_img = ImageTk.PhotoImage(self.img)
+        self.path = 'S:/Öffentliche Ordner/Logos/Core Solution/Logo/CoreSolution_Logo_RGB_negativ.png'
+        self.img = Image.open(self.path)
+        self.img.thumbnail((200,200))
+        self.new_img = ImageTk.PhotoImage(self.img)
         # Create a Label Widget to display the text or Image
-        #self.label = ttk.Label(self.Main, image = self.new_img)
-        #self.label.grid(column = 1, row = 0, padx = 5, pady = 5)
+        self.label = ttk.Label(self.Main, image = self.new_img)
+        self.label.grid(column = 1, row = 0, padx = 5, pady = 5)
 
         # ----- Section 1
         # pack options for section 1
@@ -530,14 +530,14 @@ class manager_view(ttk.Frame):
         self.Headerframe.columnconfigure(6, weight = 2)
 
         # Create an object of tkinter ImageTk
-        #self.path = 'S:/Öffentliche Ordner/Logos/Core Solution/Logo/CoreSolution_Logo_RGB_negativ.png'
-        #self.img = Image.open(self.path)
-        #self.img.thumbnail((200,200))
-        #self.new_img = ImageTk.PhotoImage(self.img)
+        self.path = 'S:/Öffentliche Ordner/Logos/Core Solution/Logo/CoreSolution_Logo_RGB_negativ.png'
+        self.img = Image.open(self.path)
+        self.img.thumbnail((200,200))
+        self.new_img = ImageTk.PhotoImage(self.img)
         # Create a Label Widget to display the text or Image
-        #self.label = ttk.Label(self.Headerframe, image = self.new_img)
-        #self.label.grid(rowspan = 2, column = 7, row = 0, \
-        #    padx = 10, pady = 20, sticky = 'e')
+        self.label = ttk.Label(self.Headerframe, image = self.new_img)
+        self.label.grid(rowspan = 2, column = 7, row = 0, \
+            padx = 10, pady = 20, sticky = 'e')
 
         self.title_label = ttk.Label(self.Headerframe, text = "Manager Request Search")
         self.title_label.configure(font = header_font)
