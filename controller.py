14f7ed6fd1bc5c92099fb4a81834e742825fefb1
rebeccafran <105347198+rebeccafran@users.noreply.github.com>
2022-07-18 13:26:35 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -255,7 +255,7 @@ class Controller:
                         for i in range(0, len(daterangelist)):
                             Controller.request_dictionary[request_number + (i * .01)] = [selected_employee, 
                                                 daterangelist[i].strftime('%Y.' + '%m.' + '%d'), status, 
-                                                stellvertreter_info, stell_status)
+                                                stellvertreter_info, stell_status]
 
     def get_holidays(self):
         Model.get_holidays(self)
